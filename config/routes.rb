cb8a90183ff51e57d790c655e3521cac351d58cb
matovu-farid <matovu90@gmail.com>
2022-05-17 14:17:14 +0300

    Add test files

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resources :recipes
-  resources :foods , except: [:show, :update, :edit]
+  resources :foods , except: [:show]
   devise_for :users
   get '/public_recipes', to: 'recipes#public'
   get '/general_shopping_list', to: 'shopping_list#index'

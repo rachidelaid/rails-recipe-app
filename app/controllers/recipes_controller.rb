2a7db756ce5a7a04320e6b65b61a0be8bf42185f
racidelaid <racidelaid@gmail.com>
2022-05-17 16:06:58 +0100

    you don't have to login to access public recipes

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,5 +1,5 @@
 class RecipesController < ApplicationController
-  before_action :authenticate_user!
+  before_action :authenticate_user!, except: [:public]
   before_action :set_recipe, only: %i[show edit update destroy]
 
   def public

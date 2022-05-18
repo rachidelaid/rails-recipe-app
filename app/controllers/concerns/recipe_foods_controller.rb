f7bc1ad4dee0f74c0f7b140a8ce8af9ecd82ef0c
racidelaid <racidelaid@gmail.com>
2022-05-18 14:15:25 +0100

    list ingredients

--- app/controllers/concerns/recipe_foods_controller.rb
+++ app/controllers/concerns/recipe_foods_controller.rb
@@ -13,8 +13,19 @@ class RecipeFoodsController < ApplicationController
       flash[:alert] = 'something happened while adding the ingredient'
     end
   end
-
+  
   def edit; end
-
-  def destroy; end
+  
+  def delete
+    p params
+    p "delete"
+    # @food = RecipeFood.find_by(id: params[:id])
+    
+    # if @food.destroy
+    #   redirect_to recipe_path(params["format"]), status: 303
+    #   flash[:notice] = 'ingredient deleted successfully'
+    # else
+    #   render :new
+    # end
+  end
 end

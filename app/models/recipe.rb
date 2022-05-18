21a639fe175994eef39a359491f9585df684e31a
matovu-farid <matovu90@gmail.com>
2022-05-18 12:41:49 +0300

    Add model tests

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -2,4 +2,8 @@ class Recipe < ApplicationRecord
   belongs_to :user
 
   validates :preparation_time, presence: true
+  validates :name,presence: true
+  validates :cooking_time, presence: true
+  validates :decription, presence:true
+  validates :public, presence: true
 end
--- spec/models/food_spec.rb
+++ spec/models/food_spec.rb
@@ -1,5 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe Food, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  it {should belong_to :user}
+  it{should validate_presence_of :name}
+  it{should validate_presence_of :measurement_unit}
+  it{should validate_presence_of :price}
 end

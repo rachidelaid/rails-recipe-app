01b5c32ee680ff0c0a3fd3cf518e492b3d8c33bc
matovu-farid <matovu90@gmail.com>
2022-05-18 17:42:33 +0300

    Add the view for the shopping list

--- app/controllers/shopping_list_controller.rb
+++ app/controllers/shopping_list_controller.rb
@@ -1,4 +1,13 @@
 class ShoppingListController < ApplicationController
   before_action :authenticate_user!
-  def index; end
+  def index
+    @amount = 3
+    @value = 1000
+    @items = [
+      {price:40,name:'apples',quantity:5},
+      {price:40,name:'apples',quantity:5},
+      {price:40,name:'apples',quantity:5},
+      {price:40,name:'apples',quantity:5}
+    ]
+  end
 end

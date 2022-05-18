1f7cef9b5f95028a36711a529eefb29bc047e89a
matovu-farid <matovu90@gmail.com>
2022-05-18 19:48:38 +0300

    Add logic for shopping food

--- app/controllers/shopping_list_controller.rb
+++ app/controllers/shopping_list_controller.rb
@@ -1,7 +1,11 @@
 class ShoppingListController < ApplicationController
   before_action :authenticate_user!
   def index
-    @amount = 3
+    available_food = Food.where(user: current_user)
+    total_recipe_food = RecipeFood.includes(food: :user).where(food: {user: current_user})
+    #author_of_the_recipe
+    missing_foods = total_recipe_food.where.not(food: available_food)
+    @amount = missing_food.count
     @value = 1000
     @items = [
       {price:40,name:'apples',quantity:5},

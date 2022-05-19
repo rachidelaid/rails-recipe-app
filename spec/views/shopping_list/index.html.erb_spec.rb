c7b9d45b8692a99d4bda15f6be8e3824625159b5
matovu-farid <matovu90@gmail.com>
2022-05-19 11:40:23 +0300

    Add Integration tests for shopping list

--- spec/views/shopping_list/index.html.erb_spec.rb
+++ spec/views/shopping_list/index.html.erb_spec.rb
@@ -4,6 +4,7 @@ RSpec.describe 'shopping_list/index', type: :system do
   let(:test_user) { create :user }
 
   before do
+    driven_by(:rack_test)
     sign_in test_user
     recipe = create :recipe
     visit shopping_list_path recipe
@@ -11,6 +12,12 @@ RSpec.describe 'shopping_list/index', type: :system do
 
   it 'should have the shopping list title' do
     expect(page).to have_content('The shopping list')
+  end
+  it 'should have the amount of food items to buy' do
+    expect(page).to have_content('Amount of food items to buy')
+  end
+  it 'should have the total value of food items needed' do
+    expect(page).to have_content('Total value of food items needed')
   end
 
   

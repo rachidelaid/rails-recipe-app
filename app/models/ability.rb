67165bae7bc02eb427519ff7eeaa704ecf57e46f
matovu-farid <matovu90@gmail.com>
2022-05-20 11:18:11 +0300

    Remove Food button

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -4,10 +4,11 @@ class Ability
   def initialize(user)
     user ||= User.new # guest user (not logged in)
     can :read, Recipe, public: true
-
+    
     return unless user.present?
 
     can :manage, Recipe, user: user
+    can :read, Food
     can :manage, Food, user: user
     can :manage, RecipeFood do |ingre|
       Recipe.find(ingre.recipe_id).user_id == user.id
